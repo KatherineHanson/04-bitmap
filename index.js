16d19dd95378a148a124ba993fb22876a8cfdb02
KatherineHanson <katherinemariehanson@gmail.com>
2017-09-17 11:43:25 -0700

    transforms

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
-const fs = require('fs')
+const fs = require('fs');
 
 // //const read = (path, callback) => {
-fs.readFile('./__test__/asset/house.bmp', (err, data) => {
+fs.readFile('./__test__/asset/bitmap.bmp', (err, data) => {
   if(err) {
-    console.error(err)
-    return 
+    console.error(err);
+    return;
   }
   //console.log('header: ', data.slice(0, 2).toString())
   //console.log('size in bytes: ', data.readUInt32LE(2, 4))
@@ -15,34 +15,61 @@ fs.readFile('./__test__/asset/house.bmp', (err, data) => {
 
   //slice out the color array and mutate it, and the original buffer is changed
   //
-  console.log('buffer: ', data)
-  console.log('type: ', data.slice(0, 2).toString())
-  console.log('size in bytes: ', data.readUInt32LE(2))
-  console.log('pixel array offset: ', data.readUInt32LE(10))
-  console.log('size of header: ', data.readUInt32LE(14))
-  console.log('width in pixels: ', data.readUInt32LE(18))
-  console.log('height in pixels: ', data.readUInt32LE(22))
-  console.log('color planes: ', data.readUInt16LE(26))
-  console.log('bits per pixel: ', data.readUInt16LE(28))
-  console.log('compression method: ', data.readUInt16LE(30))
-  console.log('image size: ', data.readUInt32LE(34))
-  console.log('horizontal resolution: ', data.readUInt32LE(38))
-  console.log('vertical resolution: ', data.readUInt32LE(42))
-  console.log('number of colors in the color palette: ', data.readUInt32LE(46))
-  console.log('number of important colors used: ', data.readUInt32LE(50))
+  console.log('buffer: ', data);
+  console.log('type: ', data.slice(0, 2).toString());
+  console.log('size in bytes: ', data.readUInt32LE(2));
+  console.log('pixel array offset: ', data.readUInt32LE(10));
+  console.log('size of header: ', data.readUInt32LE(14));
+  console.log('width in pixels: ', data.readUInt32LE(18));
+  console.log('height in pixels: ', data.readUInt32LE(22));
+  console.log('color planes: ', data.readUInt16LE(26));
+  console.log('bits per pixel: ', data.readUInt16LE(28));
+  console.log('compression method: ', data.readUInt16LE(30));
+  console.log('image size: ', data.readUInt32LE(34));
+  console.log('horizontal resolution: ', data.readUInt32LE(38));
+  console.log('vertical resolution: ', data.readUInt32LE(42));
+  console.log('number of colors in the color palette: ', data.readUInt32LE(46));
+  console.log('number of important colors used: ', data.readUInt32LE(50));
 
-  let color_table = data.slice(41, 1065)
-  for (let i = 0; i < color_table.length; i++) {
+// INVERT THE COLORS
+  // let color_table = data.slice(41, 1065);
+  // for (let i = 0; i < color_table.length; i++) {
+  //   //color_table[i] = -255;
+  //   color_table[i] = 255 - color_table[i];
+  // }
+  // console.log(color_table);
+
+// SCALE THE COLORS
+  // let color_table = data.slice(41, 1065);
+  // for (let i = 1; i < color_table.length; i+=4) {
+  //   //color_table[i] = -255;
+  //   color_table[i] = Math.round(color_table[i]/10);
+  // }
+  // console.log(color_table);
+
+// ADD NOISE TO IMAGE
+  // let color_table = data.slice(41, 1065);
+  // for (let i = 0; i < color_table.length; i++) {
+  //   if (color_table[i] === 0)
+  //     color_table[i] = Math.random() * 100;
+  //   color_table[i] = 255 - Math.random() * 100;
+  // }
+  // console.log(color_table);
+
+  //
+
+  let color_table = data.slice(41, 1065);
+  for (let i = 1; i < color_table.length; i++) {
     //color_table[i] = -255;
-    color_table[i] = 255 - color_table[i];
+    color_table[i] = color_table[i]*(1/9);
   }
-  console.log(color_table)
+  console.log(color_table);
 
-  fs.writeFile('./__test__/asset/new_house.bmp', data, (err) => {
+  fs.writeFile('./__test__/asset/new_bitmap.bmp', data, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
-})
+});
 
 // fs.writeFile('./__test__/asset/new_house.bmp', './__test__/asset/house.bmp', (err) => {
 //   if (err) throw err;
@@ -50,6 +77,5 @@ fs.readFile('./__test__/asset/house.bmp', (err, data) => {
 // });
 //}
 //  module.exports = {
-//    read: read, 
+//    read: read,
 //  };
-
